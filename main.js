15fcfad90c3fc809bac79f33ade088d1c4acbdb0
IzMichael <themichael77@outlook.com>
2020-12-03 11:16:31 +1300

    More Development

--- main.js
+++ main.js
@@ -9,6 +9,10 @@ client.once('ready', () => {
 });
 
 client.on('message', message => {
+    if (!message.channel.id == '783580937778757652' || !message.channel.id == '783556445781884928' || !message.author.bot || !message.content.startsWith(prefix)) {
+        message.channel.setPosition(0);
+    }
+
     if (!message.content.startsWith(prefix) || message.author.bot) return;
 
     const args = message.content.slice(prefix.length).split(/ +/);
@@ -16,22 +20,36 @@ client.on('message', message => {
 
     if (command === 'join') {
         let cst = message.guild.roles.cache.find(r => r.name === "Customer");
+        let everyoneRole = message.guild.roles.cache.find(role => role.name === 'Member');
 
-        message.member.roles.set([cst]);
-        
+        message.member.roles.set([cst, everyoneRole]);
+
         message.delete();
 
         message.guild.channels.create(message.author.username, {
             type: 'text',
             parent: '783541307057504286',
             topic: 'Hey! Here you can easily communicate with us, as this will be the primary method of communication.',
-        });
+            permissionOverwrites: [{
+                    id: message.author.id,
+                    allow: ['VIEW_CHANNEL'],
+                },
+                {
+                    id: '783787832589418516',
+                    deny: ['VIEW_CHANNEL'],
+                },
+            ],
+        }).then(channel => channel.send("**Hey! Here you can easily communicate with us, as this will be the primary method of communication.**"));
+    } else if (command === 'delete') {
+        message.channel.delete();
     }
 });
 
 client.on('guildMemberAdd', guildMember => {
-    let welcomeRole = guildMember.guild.roles.cache.find(role => role.name === 'New Member');
-    guildMember.roles.add(welcomeRole);
+    let newRole = guildMember.guild.roles.cache.find(role => role.name === 'New Member');
+    guildMember.roles.add(newRole);
+    let everyoneRole = guildMember.guild.roles.cache.find(role => role.name === 'Member');
+    guildMember.roles.add(everyoneRole);
 });
 
 client.login(process.env.BOT_TOKEN);
